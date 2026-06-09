68e2e064a7d018bd6797febe8f88835aea7b1e71
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-06-09 10:18:16 +0200

    Version bump to 1.0.9

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,6 @@
 <?php
 require_once "Commands/Install.php";
 // require_once "Middleware/UserCacheSession.php";
+require_once "Routes/GetSet.php";
 require_once "UserCache.php";
 require_once "PUGFunction.php";
